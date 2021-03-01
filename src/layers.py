9d18287b386a7c3b237b8f889e6b2ab1f4ddee2e
jcpagadora <joseph.pagadora.c@gmail.com>
2021-02-28 17:42:20 -0800

    context-query attention layer

--- src/layers.py
+++ src/layers.py
@@ -87,8 +87,54 @@ class EmbeddingEncoder(nn.Module):
 		return self.block1(x)
 
 
+class ContextQueryAttention(nn.Module):
+	"""This layer "combines" the context and query encoded embeddings from the
+		previous layers through attention. It is a standard context-query
+		attention layer that is used in other architectures, such as BiDAF.
 
+		For now, this was basically taken from the given starter code (BiDAF)
+	"""
+	def __init__(self, hidden_size, drop_prob=0.1):
+        super(ContextQueryAttention, self).__init__()
+        self.drop_prob = drop_prob
+        self.c_weight = nn.Parameter(torch.zeros(hidden_size, 1))
+        self.q_weight = nn.Parameter(torch.zeros(hidden_size, 1))
+        self.cq_weight = nn.Parameter(torch.zeros(1, 1, hidden_size))
+        for weight in (self.c_weight, self.q_weight, self.cq_weight):
+            nn.init.xavier_uniform_(weight)
+        self.bias = nn.Parameter(torch.zeros(1))
 
+    def forward(self, context, query, c_mask, q_mask):
+        batch_size, c_len, _ = context.size()
+        q_len = query.size(1)
+        s = self.get_similarity_matrix(context, query)        # (batch_size, c_len, q_len)
+        c_mask = c_mask.view(batch_size, c_len, 1)  # (batch_size, c_len, 1)
+        q_mask = q_mask.view(batch_size, 1, q_len)  # (batch_size, 1, q_len)
+        s1 = masked_softmax(s, q_mask, dim=2)       # (batch_size, c_len, q_len)
+        s2 = masked_softmax(s, c_mask, dim=1)       # (batch_size, c_len, q_len)
+
+        # (bs, c_len, q_len) x (bs, q_len, hid_size) => (bs, c_len, hid_size)
+        a = torch.bmm(s1, query)
+        # (bs, c_len, c_len) x (bs, c_len, hid_size) => (bs, c_len, hid_size)
+        b = torch.bmm(torch.bmm(s1, s2.transpose(1, 2)), context)
+
+        x = torch.cat([context, a, context * a, context * b], dim=2)  # (bs, c_len, 4 * hid_size)
+
+        return x
+
+    def get_similarity_matrix(self, context, query):
+        c_len, q_len = context.size(1), query.size(1)
+        c = F.dropout(c, self.drop_prob, self.training)  # (bs, c_len, hid_size)
+        q = F.dropout(q, self.drop_prob, self.training)  # (bs, q_len, hid_size)
+
+        # Shapes: (batch_size, c_len, q_len)
+        s0 = torch.matmul(c, self.c_weight).expand([-1, -1, q_len])
+        s1 = torch.matmul(q, self.q_weight).transpose(1, 2)\
+                                           .expand([-1, c_len, -1])
+        s2 = torch.matmul(c * self.cq_weight, q.transpose(1, 2))
+        s = s0 + s1 + s2 + self.bias
+
+        return s
 
 
 
--- src/model.py
+++ src/model.py
@@ -32,9 +32,11 @@ class QANet(nn.Module):
         word_vectors (torch.Tensor): Pre-trained word vectors.
         char_size (int): Number of characters in char-level vocabulary
         char_emb_dim (int): Dimensionality of character-level embedding
+        conv_dim (int): For conv layers, this is number of filters, but used throughout as hidden_size
         drop_prob (float): Dropout probability.
 	"""
-	def __init__(self, word_vectors, char_size, char_emb_dim=200, drop_prob=0.5):
+	def __init__(self, word_vectors, char_size, char_emb_dim=200, conv_dim=128, drop_prob=0.5):
+
         super(QANet, self).__init__()
         self.c_ember = layers.Embedding(word_vectors=word_vectors,
                                     char_size=char_size,
@@ -47,24 +49,25 @@ class QANet(nn.Module):
 
        	in_dim = word_vectors.size(1) + char_emb_dim
 
-        self.c_emb_encer = layers.EmbeddingEncoder(in_dim, num_conv=4, kernel=7, filters=128, num_heads=8, 
-					dropout_p=0.1, dropout=0.5, max_len=5000)
+        self.c_emb_encer = layers.EmbeddingEncoder(in_dim, num_conv=4, kernel=7, 
+        										   filters=conv_dim, num_heads=8, 
+					      						   dropout_p=0.1, dropout=0.5, 
+					      						   max_len=5000)
 
-        self.q_emb_encer = layers.EmbeddingEncoder(in_dim, num_conv=4, kernel=7, filters=128, num_heads=8, 
-					dropout_p=0.1, dropout=0.5, max_len=5000)
+        self.q_emb_encer = layers.EmbeddingEncoder(in_dim, num_conv=4, kernel=7, 
+        										   filters=conv_dim, num_heads=8, 
+												   dropout_p=0.1, dropout=0.5, 
+												   max_len=5000)
+
+        self.cq_att_layer = layers.ContextQueryAttention(conv_dim)
         
         # TODO: More layers!!
 
 
     def forward(self, cword_idxs, cchar_idxs, qword_idxs, qchar_idxs):
-    	# Might use this?
     	cword_mask = torch.zeros_like(cword_idxs) != cword_idxs
         qword_mask = torch.zeros_like(qword_idxs) != qword_idxs
         c_len_words, q_len_words = cword_mask.sum(-1), qword_mask.sum(-1)
-
-        cchar_mask = torch.zeros_like(cchar_idxs) != cchar_idxs
-        qchar_mask = torch.zeros_like(qchar_idxs) != qchar_idxs
-        c_len_chars, q_len_chars = cchar_mask.sum(-1), qchar_mask.sum(-1)
 
         # Input embedding
         c_emb = self.c_ember(cword_idxs, cchar_idxs)
@@ -73,6 +76,9 @@ class QANet(nn.Module):
         # Embedding encoder
         c_emb_enc = self.c_emb_encer(c_emb)
         q_emb_enc = self.q_emb_encer(c_emb)
+
+        # Context-Query Attention
+        cq_att = self.cq_att_layer(c_emb_enc, q_emb_enc, cword_mask, qword_mask)
 
         # TODO: Complete the rest of QANET forward computation
 
