7b44dd0620b34a2d84af3b5c50b26380902459f2
zgan1 <74687251+zgan1@users.noreply.github.com>
2021-03-02 21:59:57 -0800

    "BIDAF with char emebedding"

--- src/layers.py
+++ src/layers.py
@@ -1,3 +1,8 @@
+"""Assortment of layers for use in models.py.
+
+Author:
+    Chris Chute (chute@stanford.edu)
+"""
 
 import torch
 import torch.nn as nn
@@ -5,43 +10,92 @@ import torch.nn.functional as F
 
 from torch.nn.utils.rnn import pack_padded_sequence, pad_packed_sequence
 from util import masked_softmax
-from blocks import *
 
+
+from torch.nn.utils.rnn import pack_padded_sequence, PackedSequence
+
+"""
+def hotfix_pack_padded_sequence(input, lengths, batch_first=False, enforce_sorted=True):
+    lengths = torch.as_tensor(lengths, dtype=torch.int64)
+    lengths = lengths.cpu()
+    if enforce_sorted:
+        sorted_indices = None
+    else:
+        lengths, sorted_indices = torch.sort(lengths, descending=True)
+        sorted_indices = sorted_indices.to(input.device)
+        batch_dim = 0 if batch_first else 1
+        input = input.index_select(batch_dim, sorted_indices)
+
+    data, batch_sizes = \
+        torch._C._VariableFunctions._pack_padded_sequence(input, lengths, batch_first)
+    return PackedSequence(data, batch_sizes, sorted_indices)
+"""
 
 class Embedding(nn.Module):
-    """Input embedding layer for QANet.
+    """Embedding layer used by BiDAF, without the character-level component.
 
-   	Includes character embeddings and a 2-layer Highway Encoder
+    Word-level embeddings are further refined using a 2-layer Highway Encoder
+    (see `HighwayEncoder` class for details).
 
     Args:
         word_vectors (torch.Tensor): Pre-trained word vectors.
-        char_size (int): Number of characters in the character-vocabulary
-        char_emb_dim (int): Dimension of character embeddings
+        hidden_size (int): Size of hidden activations.
         drop_prob (float): Probability of zero-ing out activations
     """
-	def __init__(self, word_vectors, char_size, char_emb_dim, drop_prob):
+    def __init__(self, word_vectors, char_vectors, hidden_size, drop_prob):
         super(Embedding, self).__init__()
         self.drop_prob = drop_prob
         self.word_embed = nn.Embedding.from_pretrained(word_vectors)
-        self.char_embed = nn.Embedding(char_size, char_emb_dim)
+        self.char_embed = nn.Embedding.from_pretrained(char_vectors, freeze=False)
+        self.proj = nn.Linear(word_vectors.size(1) + char_vectors.size(1), hidden_size, bias=False)
+        self.conv = nn.Conv2d(char_vectors.size(1), char_vectors.size(1), kernel_size=5, padding = 5//2)
+        self.hwy = HighwayEncoder(2, hidden_size)
+
+
+    def forward(self, x, y):
+        word_emb = self.word_embed(x)   # (batch_size, seq_len, embed_size)
+        word_emb = F.dropout(word_emb, self.drop_prob, self.training)
+
+
+        # TODO 2d or 1d? do we want to use sentence info? 2d needs padding
+
+        char_emb = self.char_embed(y)
+
+        char_emb = char_emb.permute(0, 3, 1, 2)
+        char_emb = self.conv(char_emb)
+        char_emb, _ = torch.max(char_emb, dim=-1)
+        char_emb = char_emb.permute(0, 2, 1)
+
+
+        """
+
         
-        # TODO: figure out highway encoder, if this is right
-        self.hwy = HighwayEncoder(2, word_vectors.size(1) + char_emb_dim)
+   
+        char_emb = self.char_embed(y)
+        char_dim = char_emb.size()
+        char_emb = char_emb.reshape(char_dim[0]*char_dim[1], char_dim[2], char_dim[3])
+        char_emb = char_emb.permute(0, 2, 1)
+        char_emb = self.conv(char_emb)
+        char_emb, _ = torch.max(char_emb, dim=-1)
+        char_emb = char_emb.reshape(char_dim[0], char_dim[1], 64)
+        """
 
-    def forward(self, word_idxs, char_idxs):
-        word_emb = self.word_embed(word_idxs)   # (batch_size, seq_len, embed_size)
-       	char_emb = self.char_embed(char_idxs)	# (batch_size, seq_len, word_len, char_emb_dim)
-
-       	# Need to take maximum over rows of embedding matrix for each word
-       	char_emb = char_emb.max(-2).values 		# (batch_size, seq_len, char_emb_dim)
-       	# Concatenate the embedding
-       	emb = torch.cat((char_emb.T, word_emb.T)).T
-
-        emb = F.dropout(emb, self.drop_prob, self.training)
+        char_emb = F.dropout(char_emb, self.drop_prob, self.training)
+        concat = torch.cat([word_emb, char_emb], dim=-1)
+        emb = self.proj(concat)  # (batch_size, seq_len, hidden_size)
+        emb = self.hwy(emb)   # (batch_size, seq_len, hidden_size)
 
         return emb
 
-# TODO: Fill out classes for all other layers
+
+
+
+    def forward(self, x):
+        x = self.conv(x)
+        x = nn.functional.relu(x)
+        x = nn.functional.max_pool1d(x, x.shape[-1])
+
+        return torch.squeeze(x)
 
 class HighwayEncoder(nn.Module):
     """Encode an input sequence using a highway network.
@@ -68,55 +122,157 @@ class HighwayEncoder(nn.Module):
             g = torch.sigmoid(gate(x))
             t = F.relu(transform(x))
             x = g * t + (1 - g) * x
+
         return x
 
 
-class EmbeddingEncoder(nn.Module):
-	"""Embedding Encoder layer consists of a stack of encoder blocks,
-		each encoder block consists of several convolutional layers, then
-		self-attention, then a feed forward layer
-	"""
+class RNNEncoder(nn.Module):
+    """General-purpose layer for encoding a sequence using a bidirectional RNN.
 
-	def __init__(self, inp_dim, num_conv=4, kernel=7, filters=128, num_heads=8, 
-					dropout_p=0.1, dropout=0.5, max_len=5000):
-		super(EmbeddingEncoder, self).__init__()
-		self.block1 = EncoderBlock(inp_dim, num_conv, kernel, filters, num_heads, 
-					dropout_p, dropout, max_len)
+    Encoded output is the RNN's hidden state at each position, which
+    has shape `(batch_size, seq_len, hidden_size * 2)`.
 
-	def forward(self, x):
-		return self.block1(x)
+    Args:
+        input_size (int): Size of a single timestep in the input.
+        hidden_size (int): Size of the RNN hidden state.
+        num_layers (int): Number of layers of RNN cells to use.
+        drop_prob (float): Probability of zero-ing out activations.
+    """
+    def __init__(self,
+                 input_size,
+                 hidden_size,
+                 num_layers,
+                 drop_prob=0.):
+        super(RNNEncoder, self).__init__()
+        self.drop_prob = drop_prob
+        self.rnn = nn.LSTM(input_size, hidden_size, num_layers,
+                           batch_first=True,
+                           bidirectional=True,
+                           dropout=drop_prob if num_layers > 1 else 0.)
 
+    def forward(self, x, lengths):
+        # Save original padded length for use by pad_packed_sequence
+        orig_len = x.size(1)
+        # Sort by length and pack sequence for RNN
+        lengths, sort_idx = lengths.sort(0, descending=True)
+        x = x[sort_idx]     # (batch_size, seq_len, input_size)
+        x = pack_padded_sequence(x, lengths, batch_first=True)
+        # Apply RNN
+        x, _ = self.rnn(x)  # (batch_size, seq_len, 2 * hidden_size)
 
+        # Unpack and reverse sort
+        x, _ = pad_packed_sequence(x, batch_first=True, total_length=orig_len)
+        _, unsort_idx = sort_idx.sort(0)
+        x = x[unsort_idx]   # (batch_size, seq_len, 2 * hidden_size)
 
+        # Apply dropout (RNN applies dropout after all but the last layer)
+        x = F.dropout(x, self.drop_prob, self.training)
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+        return x
 
 
+class BiDAFAttention(nn.Module):
+    """Bidirectional attention originally used by BiDAF.
+
+    Bidirectional attention computes attention in two directions:
+    The context attends to the query and the query attends to the context.
+    The output of this layer is the concatenation of [context, c2q_attention,
+    context * c2q_attention, context * q2c_attention]. This concatenation allows
+    the attention vector at each timestep, along with the embeddings from
+    previous layers, to flow through the attention layer to the modeling layer.
+    The output has shape (batch_size, context_len, 8 * hidden_size).
+
+    Args:
+        hidden_size (int): Size of hidden activations.
+        drop_prob (float): Probability of zero-ing out activations.
+    """
+    def __init__(self, hidden_size, drop_prob=0.1):
+        super(BiDAFAttention, self).__init__()
+        self.drop_prob = drop_prob
+        self.c_weight = nn.Parameter(torch.zeros(hidden_size, 1))
+        self.q_weight = nn.Parameter(torch.zeros(hidden_size, 1))
+        self.cq_weight = nn.Parameter(torch.zeros(1, 1, hidden_size))
+        for weight in (self.c_weight, self.q_weight, self.cq_weight):
+            nn.init.xavier_uniform_(weight)
+        self.bias = nn.Parameter(torch.zeros(1))
+
+    def forward(self, c, q, c_mask, q_mask):
+        batch_size, c_len, _ = c.size()
+        q_len = q.size(1)
+        s = self.get_similarity_matrix(c, q)        # (batch_size, c_len, q_len)
+        c_mask = c_mask.view(batch_size, c_len, 1)  # (batch_size, c_len, 1)
+        q_mask = q_mask.view(batch_size, 1, q_len)  # (batch_size, 1, q_len)
+        s1 = masked_softmax(s, q_mask, dim=2)       # (batch_size, c_len, q_len)
+        s2 = masked_softmax(s, c_mask, dim=1)       # (batch_size, c_len, q_len)
+
+        # (bs, c_len, q_len) x (bs, q_len, hid_size) => (bs, c_len, hid_size)
+        a = torch.bmm(s1, q)
+        # (bs, c_len, c_len) x (bs, c_len, hid_size) => (bs, c_len, hid_size)
+        b = torch.bmm(torch.bmm(s1, s2.transpose(1, 2)), c)
+
+        x = torch.cat([c, a, c * a, c * b], dim=2)  # (bs, c_len, 4 * hid_size)
+
+        return x
+
+    def get_similarity_matrix(self, c, q):
+        """Get the "similarity matrix" between context and query (using the
+        terminology of the BiDAF paper).
+
+        A naive implementation as described in BiDAF would concatenate the
+        three vectors then project the result with a single weight matrix. This
+        method is a more memory-efficient implementation of the same operation.
+
+        See Also:
+            Equation 1 in https://arxiv.org/abs/1611.01603
+        """
+        c_len, q_len = c.size(1), q.size(1)
+        c = F.dropout(c, self.drop_prob, self.training)  # (bs, c_len, hid_size)
+        q = F.dropout(q, self.drop_prob, self.training)  # (bs, q_len, hid_size)
+
+        # Shapes: (batch_size, c_len, q_len)
+        s0 = torch.matmul(c, self.c_weight).expand([-1, -1, q_len])
+        s1 = torch.matmul(q, self.q_weight).transpose(1, 2)\
+                                           .expand([-1, c_len, -1])
+        s2 = torch.matmul(c * self.cq_weight, q.transpose(1, 2))
+        s = s0 + s1 + s2 + self.bias
+
+        return s
 
 
+class BiDAFOutput(nn.Module):
+    """Output layer used by BiDAF for question answering.
+
+    Computes a linear transformation of the attention and modeling
+    outputs, then takes the softmax of the result to get the start pointer.
+    A bidirectional LSTM is then applied the modeling output to produce `mod_2`.
+    A second linear+softmax of the attention output and `mod_2` is used
+    to get the end pointer.
+
+    Args:
+        hidden_size (int): Hidden size used in the BiDAF model.
+        drop_prob (float): Probability of zero-ing out activations.
+    """
+    def __init__(self, hidden_size, drop_prob):
+        super(BiDAFOutput, self).__init__()
+        self.att_linear_1 = nn.Linear(8 * hidden_size, 1)
+        self.mod_linear_1 = nn.Linear(2 * hidden_size, 1)
+
+        self.rnn = RNNEncoder(input_size=2 * hidden_size,
+                              hidden_size=hidden_size,
+                              num_layers=1,
+                              drop_prob=drop_prob)
+
+        self.att_linear_2 = nn.Linear(8 * hidden_size, 1)
+        self.mod_linear_2 = nn.Linear(2 * hidden_size, 1)
+
+    def forward(self, att, mod, mask):
+        # Shapes: (batch_size, seq_len, 1)
+        logits_1 = self.att_linear_1(att) + self.mod_linear_1(mod)
+        mod_2 = self.rnn(mod, mask.sum(-1))
+        logits_2 = self.att_linear_2(att) + self.mod_linear_2(mod_2)
+
+        # Shapes: (batch_size, seq_len)
+        log_p1 = masked_softmax(logits_1.squeeze(), mask, log_softmax=True)
+        log_p2 = masked_softmax(logits_2.squeeze(), mask, log_softmax=True)
+
+        return log_p1, log_p2
